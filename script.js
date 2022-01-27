4a63882a57f162b24951f1f8a7fcca73128bc4be
doug1231 <98552729+doug1231@users.noreply.github.com>
2022-01-27 20:18:18 -0300

    inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const msg = document.querySelector(".options");
+
+function mudaParaSim() {
+	
+	document.getElementById("imgProfile").src = "imgs/profile2.jpeg";
+	document.getElementById("msg").innerHTML = "quem ama, mama 😳";
+	
+	
+}
+
+function mudaParaNao() {
+	document.getElementById("imgProfile").src = "imgs/profile3.jpeg";
+	document.getElementById("msg").innerHTML = "tbm te amo sua fofa ❤️😛";
+	
+}
